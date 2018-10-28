3d3519583497769db4c7eb19ea4be8aca0ae4c1c
Adrian Bao <30382340+BaoAdrian@users.noreply.github.com>
2018-10-27 17:49:02 -0700

    Add files via upload

--- PCAdder/PCAdder.v
+++ PCAdder/PCAdder.v
@@ -17,6 +17,12 @@
 // (a hard-wired ADDER whose first input is from the PCResult, and 
 // whose second input is a hard-wired value of 4 that outputs PCResult + 4. 
 // The result should always be an increment of the signal 'PCResult' by 4 (i.e., PCAddResult = PCResult + 4).
+//
+// Company: University of Arizona - ECE274A - Digital Logic
+// Engineer(s): Bob Bradford, Adrian Bao
+// Comments: Implementation of an adder that adds 4 to given input
+// Last Edit: 10/27/2018 - Implemented adder functionality
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 module PCAdder(PCResult, PCAddResult);
@@ -25,7 +31,11 @@ module PCAdder(PCResult, PCAddResult);
 
     output reg [31:0] PCAddResult;
 
-    /* Please fill in the implementation here... */
+    always @ (PCResult) begin
+    
+        PCAddResult <= PCResult + 32'h00000004; // Add 4 to PCResult
+    
+    end
 
 endmodule
 
--- PCAdder/PCAdder_tb.v
+++ PCAdder/PCAdder_tb.v
@@ -5,6 +5,13 @@
 // 
 // Module - PCAdder_tb.v
 // Description - Test the 'PCAdder.v' module.
+//
+// Company: University of Arizona - ECE274A - Digital Logic
+// Engineer(s): Bob Bradford, Adrian Bao
+// Comments: Implementation of an 32-bit Adder TestBench.
+// Last Edit: 10/27/2018 - Implemented 6 Test Case to test the functionality
+//      of the 32-bit Adder. Includes 0xFFFFFFFC as stated in specifications.
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 module PCAdder_tb();
@@ -19,8 +26,24 @@ module PCAdder_tb();
     );
 
 	initial begin
+    
+    #100 PCResult <= 32'h00000007;
+    #20 $display("Result: %h", PCAddResult); 
+        
+	#100 PCResult <= 32'h000003E8;
+	#20 $display("Result: %h", PCAddResult);
 	
-    /* Please fill in the implementation here... */
+	#100 PCResult <= 32'hFFFF0000;
+    #20 $display("Result: %h", PCAddResult);
+	
+	#100 PCResult <= 32'h00000112;
+    #20 $display("Result: %h", PCAddResult);
+    
+	#100 PCResult <= 32'hFFFFFFFC;
+    #20 $display("Result: %h", PCAddResult);    
+    	
+	#100 PCResult <= 32'hFFFFFFFD;
+    #20 $display("Result: %h", PCAddResult);
 	
 	end
 
